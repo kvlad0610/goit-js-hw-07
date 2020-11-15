f994e90369064df6d5b42533254300d3be3f78f3
Vlad <kvlad0610@gmail.com>
2020-11-15 12:42:15 +0200

    Задача № 6

--- task-6.js
+++ task-6.js
@@ -1,20 +1,25 @@
-import users from './users.js';
-// Write code under this line
-const getUsersWithAge = (array, min, max) => array
- .filter(({age}) => age > min && age < max )   
- .map(({name, email}) => ({['name']: name, ['email']: email}));  
+const inputRef = document.querySelector('#validation-input');
+// console.log(inputRef)
+console.log(inputRef.dataset.length)
 
- console.log(getUsersWithAge(users, 20, 30));
-/* [
-    { name: 'Ross Vazquez', email: 'rossvazquez@xinware.com' },
-    { name: 'Elma Head', email: 'elmahead@omatom.com' },
-    { name: 'Carey Barr', email: 'careybarr@nurali.com' }
-] */
 
- console.log(getUsersWithAge(users, 30, 40));
-/* [
-    { name: 'Moore Hensley', email: 'moorehensley@indexia.com' },
-    { name: 'Sharlene Bush', email: 'sharlenebush@tubesys.com' },
-    { name: 'Blackburn Dotson', email: 'blackburndotson@furnigeer.com' },
-    { name: 'Sheree Anthony', email: 'shereeanthony@kog.com' }
-] */
+
+inputRef.addEventListener('change', event => {
+  console.log(event.target.value.length)
+  if (event.target.value.length === Number(inputRef.dataset.length)) {
+    // console.log(event.target)
+    
+    inputRef.classList.add('valid');
+    console.log(inputRef)
+  }  
+  else {
+    inputRef.classList.remove('valid')
+    inputRef.classList.add('invalid');
+    console.log(inputRef)
+  }
+})
+
+inputRef.addEventListener('focus', () => {
+  inputRef.classList.remove('invalid', 'valid')
+  console.log(inputRef)
+})
